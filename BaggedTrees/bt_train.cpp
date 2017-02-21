bb503edff0e64af632d382bc7a0b4ff8cb4fba2f
Daria Sorokina <fisharik@gmail.com>
2017-02-21 15:05:38 -0800

    minor change

--- BaggedTrees/bt_train.cpp
+++ BaggedTrees/bt_train.cpp
@@ -28,7 +28,7 @@ int main(int argc, char* argv[])
 	if((argc > 1) && !string(argv[1]).compare("-version"))
 	{
 		LogStream clog;
-		clog << "\n-----\nbt_predict ";
+		clog << "\n-----\nbt_train ";
 		for(int argNo = 1; argNo < argc; argNo++)
 			clog << argv[argNo] << " ";
 		clog << "\n\n";
